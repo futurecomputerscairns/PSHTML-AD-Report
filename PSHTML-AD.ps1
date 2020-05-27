9654f6c0ed95ad37479fd03b006f5e61c0ead607
futurecomputerscairns <44658054+futurecomputerscairns@users.noreply.github.com>
2020-05-27 11:08:47 +1000

    Update PSHTML-AD.ps1

--- PSHTML-AD.ps1
+++ PSHTML-AD.ps1
@@ -1,3 +1,37 @@
+if (-not ([System.Management.Automation.PSTypeName]'ServerCertificateValidationCallback').Type)
+{
+$certCallback = @"
+    using System;
+    using System.Net;
+    using System.Net.Security;
+    using System.Security.Cryptography.X509Certificates;
+    public class ServerCertificateValidationCallback
+    {
+        public static void Ignore()
+        {
+            if(ServicePointManager.ServerCertificateValidationCallback ==null)
+            {
+                ServicePointManager.ServerCertificateValidationCallback += 
+                    delegate
+                    (
+                        Object obj, 
+                        X509Certificate certificate, 
+                        X509Chain chain, 
+                        SslPolicyErrors errors
+                    )
+                    {
+                        return true;
+                    };
+            }
+        }
+    }
+"@
+    Add-Type $certCallback
+ }
+[ServerCertificateValidationCallback]::Ignore()
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
 <#
 .SYNOPSIS
     Generate graphed report for all Active Directory objects.
